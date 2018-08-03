3d18c32aa9cccf9f003bef254603489a394d2e6c
sevenlist <sevenlist@web.de>
2018-08-03 12:16:18 +0200

    Show bananas

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
     return (
-      <View>
-        <Text>Hello, world!</Text>
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
     );
   }
 }
