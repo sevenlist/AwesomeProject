82d9a6381e2cc113fac39da86519bdd69ca0ec97
sevenlist <sevenlist@web.de>
2018-08-03 12:22:16 +0200

    Greet using props

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+class Greeting extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
